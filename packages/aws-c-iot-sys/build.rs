b060d2e1781d1266725ba016e51d1bb1825d6d82
Simon Berger <simon.berger@inomotech.com>
2023-12-05 22:34:52 +0100

    add aws-c-iot

---
+++ packages/aws-c-iot-sys/build.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let crt_root = std::env::var("DEP_AWS_CRT_CPP_ROOT").unwrap();
+
+    println!("cargo:rerun-if-changed=aws-c-iot");
+    let out_dir = cmake::Config::new("aws-c-iot")
+        .define("CMAKE_PREFIX_PATH", crt_root)
+        .build();
+    let out_dir = out_dir.to_str().unwrap();
+
+    println!("cargo:rustc-link-search=native={out_dir}/lib");
+    println!("cargo:rustc-link-lib=static=aws-c-iot");
+}
