79ae41afa3b37e03e246a0aefe2b14bb27541ec5
Simon Berger <simon.berger@inomotech.com>
2024-01-03 21:17:42 +0100

    io cc

--- bindings/aws-c-io-sys/build.rs
+++ bindings/aws-c-io-sys/build.rs
@@ -1,58 +1,112 @@
-use std::path::{Path, PathBuf};
-
 fn main() {
-    let cmake_import_dir;
-    let mut config = aws_c_builder::Config::new("aws-c-io");
+    let config = Config::determine();
+    eprintln!("build config: {config:?}");
 
-    if aws_c_builder::is_linux_like() {
-        let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
-        cmake_import_dir = out_dir.join("_cmake_imports");
-        std::fs::create_dir_all(&cmake_import_dir).unwrap();
-
-        create_s2n_package_config(&cmake_import_dir);
-        config = config.extra_cmake_prefix_paths([cmake_import_dir.to_str().unwrap()]);
+    let mut builder = aws_c_builder2::Builder::new("aws-c-io");
+    if config.use_s2n {
+        builder.source_subdir("s2n");
+        builder.cc_callback(|build| {
+            build.define("USE_S2N", None);
+        });
     }
 
-    config
-        .aws_dependencies(["AWS_C_CAL", "AWS_C_COMMON"])
-        .bindgen_callback(|builder| {
-            builder
-                .allowlist_item("(?i)aws_async_input_stream.*")
-                .allowlist_item("(?i)aws_channel.*")
-                .allowlist_item("(?i)aws_client_bootstrap.*")
-                .allowlist_item("(?i)aws_custom_key_op_handler.*")
-                .allowlist_item("(?i)aws_event_loop.*")
-                .allowlist_item("(?i)aws_exponential_backoff.*")
-                .allowlist_item("(?i)aws_future.*")
-                .allowlist_item("(?i)aws_host.*")
-                .allowlist_item("(?i)aws_input_stream.*")
-                .allowlist_item("(?i)aws_(c_)?io.*")
-                .allowlist_item("(?i)aws_pkcs11_lib.*")
-                .allowlist_item("(?i)aws_retry.*")
-                .allowlist_item("(?i)aws_server.*")
-                .allowlist_item("(?i)aws_socket.*")
-                .allowlist_item("(?i)aws_stream.*")
-                .allowlist_item("(?i)aws_tls.*")
-                .allowlist_type("aws_address_record_type")
-                .allowlist_type("aws_generate_random_fn")
-                .allowlist_type("aws_new_event_loop_fn")
-                .allowlist_type("aws_standard_retry_options")
-                .allowlist_type("aws.+host.+_fn")
-        })
+    builder
+        .source_subdirs(config.source_dirs.iter().copied())
+        .cc_callback(|build| config.event_loop.set_as_define(build))
+        .dependencies(["AWS_C_CAL", "AWS_C_COMMON"])
+        .bindings_suffix(config.bindings.suffix())
         .build();
 }
 
-fn create_s2n_package_config(target_dir: &Path) {
-    use std::fmt::Write;
+#[derive(Debug, Default)]
+struct Config {
+    use_s2n: bool,
+    event_loop: EventLoop,
+    source_dirs: &'static [&'static str],
+    bindings: Bindings,
+}
 
-    let s2n_include = std::env::var("DEP_S2N_TLS_INCLUDE").unwrap();
+impl Config {
+    fn determine() -> Self {
+        let target_family = std::env::var("CARGO_CFG_TARGET_FAMILY").unwrap();
+        if target_family == "windows" {
+            return Self {
+                use_s2n: false,
+                event_loop: EventLoop::IoCompletionPorts,
+                bindings: Bindings::Win32IoCompletionPorts,
+                source_dirs: &["windows", "windows/iocp"],
+            };
+        }
 
-    let mut content = String::new();
-    writeln!(&mut content, "add_library(AWS::s2n STATIC IMPORTED)").unwrap();
-    writeln!(
-        &mut content,
-        "target_include_directories(AWS::s2n INTERFACE \"{s2n_include}\")"
-    )
-    .unwrap();
-    std::fs::write(target_dir.join("s2n-config.cmake"), content).unwrap();
+        let target_vendor = std::env::var("CARGO_CFG_TARGET_VENDOR").unwrap();
+        if target_vendor == "apple" {
+            return Self {
+                use_s2n: false,
+                event_loop: EventLoop::KQueue,
+                bindings: Bindings::Apple,
+                source_dirs: &["bsd", "posix", "darwin"],
+            };
+        }
+
+        let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+        match target_os.as_str() {
+            "linux" | "android" => Self {
+                use_s2n: true,
+                event_loop: EventLoop::EPoll,
+                bindings: Bindings::Generic,
+                source_dirs: &["linux", "posix"],
+            },
+            "freebsd" | "openbsd" | "netbsd" => Self {
+                use_s2n: true,
+                event_loop: EventLoop::KQueue,
+                bindings: Bindings::Generic,
+                source_dirs: &["bsd", "posix"],
+            },
+            _ => Self::default(),
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+enum EventLoop {
+    #[default]
+    None,
+    IoCompletionPorts,
+    EPoll,
+    KQueue,
+}
+
+impl EventLoop {
+    fn define_value(self) -> Option<&'static str> {
+        match self {
+            Self::None => None,
+            Self::IoCompletionPorts => Some("IO_COMPLETION_PORTS"),
+            Self::EPoll => Some("EPOLL"),
+            Self::KQueue => Some("KQUEUE"),
+        }
+    }
+
+    fn set_as_define(self, build: &mut aws_c_builder2::cc::Build) {
+        if let Some(value) = self.define_value() {
+            build.define(&format!("AWS_USE_{value}"), None);
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default)]
+enum Bindings {
+    #[default]
+    Generic,
+    Win32IoCompletionPorts,
+    Apple,
+}
+
+impl Bindings {
+    fn suffix(self) -> &'static str {
+        match self {
+            Self::Generic => "generic",
+            Self::Win32IoCompletionPorts => "win32_iocp",
+            Self::Apple => "apple",
+        }
+    }
 }
--- bindings/aws-c-io-sys/src/lib.rs
+++ bindings/aws-c-io-sys/src/lib.rs
@@ -2,24 +2,19 @@
 //! protocols.
 
 #![no_std]
+#![allow(
+    clippy::all,
+    clippy::wildcard_imports,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+    rustdoc::all
+)]
 
 extern crate aws_c_cal_sys;
 extern crate aws_c_common_sys;
 
-mod bindings {
-    #![allow(
-        clippy::all,
-        clippy::wildcard_imports,
-        non_camel_case_types,
-        non_snake_case,
-        non_upper_case_globals,
-        rustdoc::all
-    )]
+use aws_c_common_sys::*;
+use libc::*;
 
-    use aws_c_common_sys::*;
-    use libc::*;
-
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
-
-pub use bindings::*;
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
