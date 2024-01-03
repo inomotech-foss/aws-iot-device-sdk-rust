bc7e302944cb9d0a99b03123adb10e041db92833
Simon Berger <simon.berger@inomotech.com>
2024-01-03 08:42:36 +0100

    event stream

--- bindings/aws-c-event-stream-sys/build.rs
+++ bindings/aws-c-event-stream-sys/build.rs
@@ -1,6 +1,7 @@
+use std::path::Path;
+
 fn main() {
-    aws_c_builder::Config::new("aws-c-event-stream")
-        .aws_dependencies(["AWS_C_COMMON", "AWS_C_IO", "AWS_CHECKSUMS"])
-        .bindgen_callback(|builder| builder.allowlist_item("(?i)aws_(c_)?event_stream.*"))
+    aws_c_builder2::Builder::new(Path::new("aws-c-event-stream"))
+        .dependencies(["AWS_C_CHECKSUMS", "AWS_C_COMMON", "AWS_C_IO"])
         .build();
 }
